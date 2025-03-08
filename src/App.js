ab2eaecb51632f44f43cb90a7df740f75d8e86f0
FazleyRM <frahaman520@gmail.com>
2025-03-08 11:04:24 +0530

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import Search from './Search';
-import Stories from './Stories';
-import Pagination from './Pagination';
+import React from "react";
+import Navbar from "./Navbar";
+import Search from "./Search";
+import Stories from "./Stories";
+import Pagination from "./Pagination";
 
-import './App.css';
+import "./App.css";
+import "./Navbar.css";
 
-
-
-export const App = () => {
-
+const App = () => {
   return (
-     
     <>
-   
-    <Search/>
-    <Pagination/>
-    <Stories/>
-   
+      <Navbar /> {/* Navbar should be here */}
+      <Search />
+      <Pagination />
+      <Stories />
     </>
-    
-  )
-}
+  );
+};
 
 export default App;
